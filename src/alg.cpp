7623e4e115e434105e527714d182320d9d10b2a5
senseynn <163774648+senseynn@users.noreply.github.com>
2026-03-27 10:29:15 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,10 +1,76 @@
 // Copyright 2021 NNTU-CS
+#include "alg.h"
+
 int countPairs1(int *arr, int len, int value) {
-  return 0;
+    int count = 0;
+    for (int i = 0; i < len; i++) {
+        for (int j = i + 1; j < len; j++) {
+            if (arr[i] + arr[j] == value) {
+                count++;
+                while (j + 1 < len && arr[j] == arr[j + 1]) {
+                    j++;
+                }
+            }
+        }
+        while (i + 1 < len && arr[i] == arr[i + 1]) {
+            i++;
+        }
+    }
+    return count;
 }
+
 int countPairs2(int *arr, int len, int value) {
-  return 0;
+    int count = 0;
+    for (int i = 0; i < len; i++) {
+        int target = value - arr[i];
+        int left = i + 1;
+        int right = len - 1;
+        int found = -1;
+
+        while (left <= right) {
+            int mid = (left + right) / 2;
+            if (arr[mid] == target) {
+                found = mid;
+                break;
+            } else if (arr[mid] < target) {
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+        }
+
+        if (found != -1) {
+            count++;
+            while (i + 1 < len && arr[i] == arr[i + 1]) {
+                i++;
+            }
+        }
+    }
+    return count;
 }
+
 int countPairs3(int *arr, int len, int value) {
-  return 0;
+    int count = 0;
+    int left = 0;
+    int right = len - 1;
+
+    while (left < right) {
+        int sum = arr[left] + arr[right];
+        if (sum == value) {
+            count++;
+            int leftVal = arr[left];
+            while (left < right && arr[left] == leftVal) {
+                left++;
+            }
+            int rightVal = arr[right];
+            while (left <= right && arr[right] == rightVal) {
+                right--;
+            }
+        } else if (sum < value) {
+            left++;
+        } else {
+            right--;
+        }
+    }
+    return count;
 }
